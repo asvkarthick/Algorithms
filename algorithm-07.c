020cae4cbd286aead1a0519f5162fb9541dacf06
Karthick Kumaran <asvkarthick@gmail.com>
2018-07-13 22:34:53 -0700

    Added program to compress input string

---
+++ algorithm-07.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+    Algorithm:
+
+    Implement a function to perform basic string compression using the counts
+    of repeated characters. For example, the string aabcccccaaa would become
+    a2b1c5a3. If the compressed string would not become smaller than the original
+    string, your function should return the original string. You can assume the
+    string has only uppercase and lowercase letters (a-z)
+*/
+
+char *compress(char *string)
+{
+    char *compressed_string = strdup(string);
+    int string_length = strlen(string);
+    int character_count = 1;
+    int i, j = 0;
+    char previous_character = string[0];
+
+    memset(compressed_string, '\0', string_length);
+
+    for(i = 1; i < string_length; i++)
+    {
+        if(previous_character == string[i])
+        {
+            character_count++;
+        }
+        else
+        {
+            if(j + 2 >= string_length)
+            {
+                return string;
+            }
+            compressed_string[j++] = previous_character;
+            compressed_string[j++] = '0' + character_count;
+            previous_character = string[i];
+            character_count = 1;
+        }
+    }
+
+    if(j + 2 >= string_length)
+    {
+        return string;
+    }
+    else
+    {
+        compressed_string[j++] = previous_character;
+        compressed_string[j++] = '0' + character_count;
+    }
+
+    return compressed_string;
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc < 2)
+    {
+        printf("Usage: %s <string>\n", argv[0]);
+        return -1;
+    }
+
+    printf("Compressed string of %s is %s\n", argv[1], compress(argv[1]));
+
+    return 0;
+}
