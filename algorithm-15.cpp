85f5df668a1c1d35b123b877e41cb57a87d1f738
Karthick Kumaran <asvkarthick@gmail.com>
2018-07-22 00:30:25 -0700

    Updated reverse string program

--- algorithm-15.cpp
+++ algorithm-15.cpp
@@ -10,7 +10,6 @@ void ReverseString(std::string& s, int i)
     c = s.at(i);
     ReverseString(s, i + 1);
     s.at(s.length() - 1 - i) = c;
-    std::cout << c << " " << i << std::endl;
 }
 
 int main(void)
@@ -18,16 +17,9 @@ int main(void)
     std::string s { "Karthick" };
     int i;
 
-    for(i = 0; i < s.length(); i++)
-        std::cout << s.at(i) << std::endl;
-
-    std::cout << std::endl;
+    std::cout << "Original string: " << s << std::endl;
     ReverseString(s, 0);
-    std::cout << std::endl;
-
-
-    for(i = 0; i < s.length(); i++)
-        std::cout << s.at(i) << std::endl;
+    std::cout << "Reversed string: " << s << std::endl;
 
     return 0;
 }
