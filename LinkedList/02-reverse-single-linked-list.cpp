1899767836d588393af47417afe5ea1227bee9d3
Karthick Kumaran <asvkarthick@gmail.com>
2018-07-22 11:23:45 -0700

    Added function to reverse single linked list

---
+++ LinkedList/02-reverse-single-linked-list.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+
+class Node
+{
+public:
+    Node() = delete;
+
+    Node(int d) : next(nullptr), data(d) {}
+
+    Node *next;
+    int data;
+};
+
+class SingleLinkedList
+{
+public:
+    SingleLinkedList() : head(nullptr) {}
+
+    Node *GetNode(int data)
+    {
+        return new Node(data);
+    }
+
+    void PrintList()
+    {
+        Node *node = head;
+
+        while(node != nullptr)
+        {
+            std::cout << node->data << " -> ";
+            node = node->next;
+        }
+        std::cout << "NULL" << std::endl;
+    }
+
+    void AddNode(int data)
+    {
+        Node *node = GetNode(data);
+
+        if(head == nullptr)
+        {
+            head = node;
+        }
+        else
+        {
+            tail->next = node;
+        }
+        tail = node;
+    }
+
+    void ReverseList()
+    {
+        Node *prev, *next, *node = head;
+
+        prev = nullptr;
+        while(node != nullptr)
+        {
+            next = node->next;
+            node->next = prev;
+            prev = node;
+            node = next;
+        }
+        head = prev;
+    }       
+
+    ~SingleLinkedList()
+    {
+        Node *prev, *node = head;
+
+        while(node != nullptr)
+        {
+            prev = node;
+            node = node->next;
+            delete prev;
+        }
+
+        head = tail = nullptr;
+    }
+
+private:
+    Node *head;
+    Node *tail;
+};
+
+int main(void)
+{
+    SingleLinkedList *s;
+
+    s = new SingleLinkedList();
+
+    s->AddNode(10);
+    s->AddNode(20);
+    s->AddNode(30);
+    s->PrintList();
+    s->ReverseList();
+    s->PrintList();
+
+    delete s;
+
+    return 0;
+}
