e7a883f8f06c49af2acd7a158b81655a4465ac3f
Karthick Kumaran <asvkarthick@gmail.com>
2018-07-21 19:27:52 -0700

    Added program to make change

---
+++ DynamicProgramming/05-makechange.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/*
+    Algorithm:
+
+    Given an integer representing a given amount of change, write a function to compute
+    the total number of coins required to make that amount of change.
+
+    Assume American coins (1, 5, 10 and 25 cents)
+*/
+
+int makechange(int *coins, int n, int change)
+{
+    int number_of_coins = 0, accumulated = 0;
+
+    n--;
+    while(change != accumulated)
+    {
+        if(coins[n] + accumulated <= change)
+        {
+            accumulated += coins[n];
+            number_of_coins++;
+        }
+        else
+        {
+            n--;
+        }
+    }
+
+    return number_of_coins;
+}
+
+int main(void)
+{
+    int n, change;
+    int coins[] = { 1, 5, 10, 25 };
+
+    n = sizeof(coins) / sizeof(coins[0]);
+
+    printf("Enter value to change: ");
+    scanf("%d", &change);
+    printf("Number of coins to change %d = %d\n", change, makechange(coins, n, change));
+
+    return 0;
+}
