e60a5992f37ca00245e284e4f104f8ccc7f98b98
Karthick Kumaran <asvkarthick@gmail.com>
2018-07-24 02:31:04 -0700

    Added iterator support to print elements

---
+++ C++STL/05-set/set-03.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <set>
+
+// Simple set program to insert and print elements
+// This program demonstrates that the order of elements that stored in
+// the set is sorted and also removes duplicates
+
+int main(void)
+{
+    std::set<int> s;
+    int i;
+
+    for(i = 10; i > 0; i--)
+    {
+        std::cout << "Insert " << i << std::endl;
+        s.insert(i);
+    }
+    for(i = 10; i > 0; i--)
+    {
+        std::cout << "Insert " << i << std::endl;
+        s.insert(i);
+    }
+
+    std::cout << "Printing set elements using range based for loop" << std::endl;
+    for(auto x : s)
+        std::cout << x << std::endl;
+
+    std::cout << "Printing set elements using iterator" << std::endl;
+    for(auto x = s.begin(); x != s.end(); x++)
+        std::cout << *x << std::endl;
+
+    return 0;
+}
