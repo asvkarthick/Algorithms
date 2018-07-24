05b1a4a7423f3b2dd55c8fabbbb78652770e4e64
Karthick Kumaran <asvkarthick@gmail.com>
2018-07-24 01:44:01 -0700

    Added list program to add and print elements using iterator

---
+++ C++STL/04-list/list-03.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <list>
+
+// Simple list program to push and print elements
+// This program has been written to show that the list elements cannot be accessed randomly like array
+// This program will not compile as list elements cannot be accessed randomly
+
+int main(void)
+{
+    std::list<int> l;
+    int i;
+
+    for(i = 0; i < 10; i++)
+        l.push_back(i);
+
+    std::cout << "Print list elements using range based for loop" << std::endl;
+    for(auto x : l)
+        std::cout << x << std::endl;
+
+    std::cout << "Print list elements using iterator" << std::endl;
+    for(auto x = l.begin(); x != l.end(); x++)
+        std::cout << *x << std::endl;
+
+    return 0;
+}
