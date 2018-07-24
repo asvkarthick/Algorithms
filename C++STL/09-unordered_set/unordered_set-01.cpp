dd0901e596eb107cf49c1ff7758ac414d5b9b575
Karthick Kumaran <asvkarthick@gmail.com>
2018-07-24 13:13:04 -0700

    Simple program to demonstrate unordered set

---
+++ C++STL/09-unordered_set/unordered_set-01.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unordered_set>
+
+// Simple unordered_set program to insert and print elements
+// This program demonstrates that the order of elements that stored in
+// the unordered set is random
+
+int main(void)
+{
+    std::unordered_set<int> s;
+    int i;
+
+    for(i = 10; i > 0; i--)
+    {
+        std::cout << "Insert " << i << std::endl;
+        s.insert(i);
+    }
+
+    for(auto x : s)
+        std::cout << x << std::endl;
+
+    return 0;
+}
