bdde83197df73038fc9790dfd2faa5d922732cda
Karthick Kumaran <asvkarthick@gmail.com>
2018-07-24 14:13:59 -0700

    Another map program to add elements dynamically

---
+++ C++STL/07-map/map-05.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+
+// Simple map program to demonstrate that it cannot keep duplicates
+// but stores the keys in sorted order
+
+int main(void)
+{
+    std::map<int, int> m;
+
+    // insert some elements in arbitrary order
+    for(int i = 0; i < 10; i++)
+        m[i] = 10 - i;
+
+    std::cout << "Printing map using range based for loop" << std::endl;
+    for(const auto& x : m)
+        std::cout << x.first << " : " << x.second << std::endl;
+
+    std::cout << "Printing map using iterators" << std::endl;
+    for(auto x = m.begin(); x != m.end(); x++)
+        std::cout << x->first << " : " << x->second << std::endl;
+
+    std::cout << "Printing map using const iterators" << std::endl;
+    for(auto x = m.cbegin(); x != m.cend(); x++)
+        std::cout << x->first << " : " << x->second << std::endl;
+
+    return 0;
+}
