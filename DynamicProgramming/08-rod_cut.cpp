ddab5130564776ab6a7889d6a8291c62a7387943
Karthick Kumaran <asvkarthick@gmail.com>
2018-07-27 23:44:20 -0700

    Dynamic program to find the maximum revenue by cutting rods

---
+++ DynamicProgramming/08-rod_cut.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int rod_cut(int *p, int n)
+{
+    if(n == 0)
+        return 0;
+
+    int q = 0;
+
+    for(int i = 1; i <= n; i++)
+        q = std::max(q, p[i] + rod_cut(p, n - i));
+
+    return q;
+}
+
+int main(void)
+{
+    int p[] = { 0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+    int optimal_revenue[10];
+
+    for(int i = 0; i < 10; i++)
+    {
+        optimal_revenue[i] = rod_cut(p, i + 1);
+        std::cout << "Revenue[" << i << "] = " << optimal_revenue[i] << std::endl;
+    }
+
+    return 0;
+}
