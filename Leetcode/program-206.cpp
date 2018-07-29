c7320d7b5c666d4ee0a98f03a3f5f26d64e3ea5f
Karthick Kumaran <asvkarthick@gmail.com>
2018-07-29 01:24:43 -0700

    Added main to reverse linked list program

--- Leetcode/program-206.cpp
+++ Leetcode/program-206.cpp
@@ -23,6 +23,13 @@ A linked list can be reversed either iteratively or recursively. Could you imple
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
@@ -37,5 +44,34 @@ public:
         
         return prev;
     }
+
+    void printNodes(ListNode* head)
+    {
+        while(head != nullptr) {
+            std::cout << head->val << " -> ";
+            head = head->next;
+        }
+        std::cout << "NULL" << std::endl;
+    }
 };
 
+int main(void)
+{
+    Solution s;
+    ListNode *head;
+
+    head = new ListNode(10);
+    head->next = new ListNode(20);
+    head->next->next = new ListNode(30);
+    head->next->next->next = new ListNode(40);
+    head->next->next->next->next = new ListNode(50);
+
+    std::cout << "Original list:" << std::endl;
+    s.printNodes(head);
+
+    ListNode *reverse = s.reverseList(head);
+    std::cout << "Reversed list:" << std::endl;
+    s.printNodes(reverse);
+
+    return 0;
+}
