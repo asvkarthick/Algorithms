181f30b0373e49f713d784a7cacd9d5c047f51eb
Karthick Kumaran <asvkarthick@gmail.com>
2018-08-05 14:02:19 -0700

    Modified rectangle area program

--- Leetcode/program-223.cpp
+++ Leetcode/program-223.cpp
@@ -3,13 +3,34 @@
 class Solution {
 public:
     int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        int area1, area2, area3 = 0;
+        int area1, area2, area3 = 0, area, x, y;
         
         area1 = (C - A) * (D - B);
         area2 = (G - E) * (H - F);
-        if(C > E && H > B)
-            area3 = (C - E) * (H - B);
         
-        return area1 + area2 - area3;
+        if(A < G && B < H && E < C && F < D)
+        {
+            // Overlap
+            if(A < E)
+                x = C - E;
+            else
+                x = G - A;
+            
+            if(B < F)
+                y = D - F;
+            else
+                y = H - B;
+            
+            area3 = x * y;
+            
+            area = area1 + area2 - area3;
+        }
+        else
+        {
+            // No overlap
+            area = area1 + area2;
+        }
+        
+        return area;
     }
 };
