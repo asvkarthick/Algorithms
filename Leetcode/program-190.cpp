7e340e3eaa662840eac3b28773b3beed6068a43f
Karthick Kumaran <asvkarthick@gmail.com>
2018-08-05 23:32:54 -0700

    Added program to reverse bits

---
+++ Leetcode/program-190.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+/*
+Reverse bits of a given 32 bits unsigned integer.
+
+Example:
+
+Input: 43261596
+Output: 964176192
+Explanation: 43261596 represented in binary as 00000010100101000001111010011100, 
+             return 964176192 represented in binary as 00111001011110000010100101000000.
+Follow up:
+If this function is called many times, how would you optimize it?
+*/
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+        int i;
+
+        for(i = 0; i < sizeof(n) * 8; i++)
+        {
+            result = (result << 1) | (n & 1);
+            n = n >> 1;
+        }
+
+        return result;
+    }
+};
