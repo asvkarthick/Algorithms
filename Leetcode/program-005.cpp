e84c1548ff5f3f6f756ba4e20030bddbfdc527a2
Karthick Kumaran <asvkarthick@gmail.com>
2018-08-09 12:21:35 -0700

    Longest palindrome substring

--- Leetcode/program-005.cpp
+++ Leetcode/program-005.cpp
@@ -1,48 +1,20 @@
 #include <iostream>
+#include <string>
 
 /*
 
-Implement atoi which converts a string to an integer.
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
 
-The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
-
-The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
-
-If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
-
-If no valid conversion could be performed, a zero value is returned.
-
-Note:
-
-Only the space character ' ' is considered as whitespace character.
-Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [-2^31,  2^31 - 1]. If the numerical value is out of the range of representable values, INT_MAX (2^31 - 1) or INT_MIN (-2^31) is returned.
 Example 1:
 
-Input: "42"
-Output: 42
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+
 Example 2:
 
-Input: "   -42"
-Output: -42
-Explanation: The first non-whitespace character is '-', which is the minus sign.
-             Then take as many numerical digits as possible, which gets 42.
-Example 3:
-
-Input: "4193 with words"
-Output: 4193
-Explanation: Conversion stops at digit '3' as the next character is not a numerical digit.
-Example 4:
-
-Input: "words and 987"
-Output: 0
-Explanation: The first non-whitespace character is 'w', which is not a numerical 
-             digit or a +/- sign. Therefore no valid conversion could be performed.
-Example 5:
-
-Input: "-91283472332"
-Output: -2147483648
-Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer.
-             Thefore INT_MIN (-2^31) is returned.
+Input: "cbbd"
+Output: "bb"
 
 */
 
@@ -50,65 +22,66 @@ using namespace std;
 
 class Solution {
 public:
-    int myAtoi(string str) {
-        int string_length = str.length();
-        int i, number, sign = 1;
-        int integer = 0;
-        bool isFirstSign = true;
-
-        for(i = 0; i < string_length; i++)
+    int palindromeLength(std::string& s, int mid1, int mid2)
+    {
+        while(mid1 >= 0 && mid2 < s.length() && s.at(mid1) == s.at(mid2))
         {
-            char ch = str.at(i);
-            if(ch >= '0' && ch <= '9')
-            {
-                number = ch - '0';
-            }
-            else if(ch == '-')
-            {
-                sign = -1;
-                if(!isFirstSign)
-                    return sign * integer;
-                isFirstSign = false;
-                continue;
-            }
-            else if(ch == ' ')
-            {
-                continue;
-            }
-            else if(ch == '+')
-            {
-                if(!isFirstSign)
-                    return sign * integer;
-                isFirstSign = false;
-                continue;
-            }
-            else
-            {
-                return sign * integer;
-            }
+            mid1--;
+            mid2++;
+        }
+        
+        return mid2 - mid1 - 1;
+    }
 
-            if(integer > (0x7FFFFFFF / 10))
+    string longestPalindrome(string s) {
+        int len = s.length();
+        int max_len = 0, i,l, start = 0;
+        std::string palindrome;
+
+        if(len <= 1)
+            return s;
+        
+        for(i = 0; i < len; i++)
+        {
+            // For odd length palindrome
+            l = palindromeLength(s, i, i);
+            if(l > max_len)
             {
-                if(sign == 1)
-                    return 0x7FFFFFFF;
-                else
-                    return 0x80000000;
+                max_len = l;
+                start = i - (l / 2);
+                // std::cout << "Odd Start = " << start << " l = " << l << " i = " << i << std::endl;
             }
-            integer = integer * 10 + ch - '0';
+            
+            // For even length palindrome
+            l = palindromeLength(s, i, i + 1);
+            if(l > max_len)
+            {
+                max_len = l;
+                start = i - (l / 2) + 1;
+                // std::cout << "Even Start = " << start << " l = " << l << " i = " << i << std::endl;
+            }
         }
 
-        return sign * integer;
+        len = 0;
+        while(len < max_len)
+        {
+            palindrome.push_back(s.at(start));
+            ++start;
+            ++len;
+        }
+
+        return palindrome;
     }
 };
 
 int main(void)
 {
     Solution s;
-    std::cout << s.myAtoi(std::string { "42" }) << std::endl;
-    std::cout << s.myAtoi(std::string { "   -42" }) << std::endl;
-    std::cout << s.myAtoi(std::string { "4193 with words" }) << std::endl;
-    std::cout << s.myAtoi(std::string { "words and 987" }) << std::endl;
-    std::cout << s.myAtoi(std::string { "-91283472332" }) << std::endl;
+    std::string str1 { "babad" };
+    std::string str2 { "cbbd" };
+
+    std::cout << s.longestPalindrome(str1) << std::endl;
+    std::cout << s.longestPalindrome(str2) << std::endl;
 
     return 0;
 }
