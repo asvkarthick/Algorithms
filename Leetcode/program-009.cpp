4a26b112a76acc745d665169f80e8530342f00c9
Karthick Kumaran <asvkarthick@gmail.com>
2018-08-09 20:52:53 -0700

    Added palindrome program

---
+++ Leetcode/program-009.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+/*
+
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+
+Input: 121
+Output: true
+
+Example 2:
+
+Input: -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Example 3:
+
+Input: 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+Follow up:
+
+Coud you solve it without converting the integer to a string?
+
+*/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int n = x, reverse = 0;
+        
+        if(x < 0)
+            return false;
+        
+        while(n > 0)
+        {
+            reverse = (reverse * 10) + (n % 10);
+            n = n / 10;
+        }
+        
+        return reverse == x;
+    }
+};
+
+int main(void)
+{
+    Solution s;
+
+    std::cout << s.isPalindrome(121) << std::endl;
+    std::cout << s.isPalindrome(-121) << std::endl;
+    std::cout << s.isPalindrome(10) << std::endl;
+
+    return 0;
+}
