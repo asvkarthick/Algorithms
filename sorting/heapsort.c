7e856b64d356a9e2d8a8acb7cd29b70a9427a8ab
Karthick Kumaran <asvkarthick@gmail.com>
2018-08-16 21:46:25 -0700

    Added both min and max heap

--- sorting/heapsort.c
+++ sorting/heapsort.c
@@ -36,18 +36,34 @@ void minHeapify(int *a, int n, int heap_size)
     }
 }
 
-void heapsort(int *a, int n)
+void maxHeapify(int *a, int n, int heap_size)
+{
+    int largest = n;
+    int l = left(n);
+    int r = right(n);
+
+    if(l < heap_size && a[n] < a[l])
+        largest = l;
+    if(r < heap_size && a[largest] < a[r])
+        largest = r;
+
+    if(largest != n)
+    {
+        int tmp = a[largest];
+        a[largest] = a[n];
+        a[n] = tmp;
+        maxHeapify(a, largest, heap_size);
+    }
+}
+
+void heapsort_descending(int *a, int n)
 {
     int i;
     int heap_size = n;
 
     // Build min heap
     for(i = n / 2; i >= 0; i--)
-    {
-        printf("a[%d] = %d\n", i, a[i]);
         minHeapify(a, i, n);
-        printf("a[%d] = %d\n", i, a[i]);
-    }
 
     for(i = n - 1; i >= 1; i--)
     {
@@ -60,9 +76,29 @@ void heapsort(int *a, int n)
     }
 }
 
+void heapsort_ascending(int *a, int n)
+{
+    int i;
+    int heap_size = n;
+
+    // Build max heap
+    for(i = n / 2; i >= 0; i--)
+        maxHeapify(a, i, n);
+
+    for(i = n - 1; i >= 1; i--)
+    {
+        int tmp = a[i];
+        a[i] = a[0];
+        a[0] = tmp;
+
+        --heap_size;
+        maxHeapify(a, 0, heap_size);
+    }
+}
+
 int main(void)
 {
-    // int a[] = { 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+    int b[] = { 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
     int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
     int i;
 
@@ -71,11 +107,23 @@ int main(void)
         printf("%d ", a[i]);
     printf("\n\n");
 
-    heapsort(a, sizeof(a) / sizeof(a[0]));
+    heapsort_descending(a, sizeof(a) / sizeof(a[0]));
 
     printf("After sorting...\n");
     for(i = 0; i < sizeof(a) / sizeof(a[0]); i++)
         printf("%d ", a[i]);
+    printf("\n\n");
+
+    printf("Before sorting...\n");
+    for(i = 0; i < sizeof(b) / sizeof(b[0]); i++)
+        printf("%d ", b[i]);
+    printf("\n\n");
+
+    heapsort_ascending(b, sizeof(b) / sizeof(b[0]));
+
+    printf("After sorting...\n");
+    for(i = 0; i < sizeof(b) / sizeof(b[0]); i++)
+        printf("%d ", b[i]);
     printf("\n\n");
 
     return 0;
