048f6147ee70d50823b39fdb1ced6ebbe2862853
Karthick Kumaran <asvkarthick@gmail.com>
2021-09-14 16:09:07 -0700

    Reverse linked list using recursion
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ Recursion/07-reverseLinkedList.cpp
@@ -0,0 +1,56 @@
+/* Program: Reverse linked list using recursion
+ * Weblink: https://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1681/
+ * Author : Karthick Kumaran
+ * E-mail : asvkarthick@gmail.com
+ */
+
+#include <iostream>
+using namespace std;
+
+struct ListNode
+{
+        int data;
+        ListNode *next;
+};
+
+void printList(ListNode *l)
+{
+        while (l != nullptr) {
+                std::cout << l->data << " -> ";
+                l = l->next;
+        }
+        std::cout << "NULL" << std::endl;
+}
+
+ListNode* reverse(ListNode *head, ListNode *prev)
+{
+        if (head == nullptr) return nullptr;
+        if (head->next == nullptr) {
+                head->next = prev;
+                return head;
+        }
+        ListNode *next = head->next;
+        head->next = prev;
+        return reverse(next, head);
+}
+
+int main() {
+        ListNode *head;
+        head = new ListNode;
+        head->data = 10;
+        head->next = new ListNode;
+        head->next->data = 20;
+        head->next->next = new ListNode;
+        head->next->next->data = 30;
+        head->next->next->next = new ListNode;
+        head->next->next->next->data = 40;
+        head->next->next->next->next = new ListNode;
+        head->next->next->next->next->data = 50;
+        head->next->next->next->next->next = nullptr;
+
+        printList(head);
+        head = reverse(head, nullptr);
+        printList(head);
+
+        return 0;
+}
