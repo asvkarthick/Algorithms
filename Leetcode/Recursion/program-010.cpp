55d1619c5814b0b0ea18f5b5aa833e7a97927990
Karthick Kumaran <asvkarthick@gmail.com>
2021-09-15 00:29:27 -0700

    Merge two sorted lists using recursion
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ Leetcode/Recursion/program-010.cpp
@@ -0,0 +1,30 @@
+/* Problem: Merge Two Sorted Lists using recursion
+ * Weblink: https://leetcode.com/explore/learn/card/recursion-i/253/conclusion/2382/
+ * Author : Karthick Kumaran
+ * E-mail : asvkarthick@gmail.com
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr) return l2;
+        else if (l2 == nullptr) return l1;
+        if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+};
