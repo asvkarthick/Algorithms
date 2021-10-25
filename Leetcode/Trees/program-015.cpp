d9072b1bcab448160ae644966e3af52566c5c059
Karthick Kumaran <asvkarthick@gmail.com>
2021-10-25 02:40:11 -0700

    Serialize / Deserialize Binary Tree
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ Leetcode/Trees/program-015.cpp
@@ -0,0 +1,74 @@
+/* Program: Serialize / Deserialize Binary Tree
+ * Weblink: https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+ * Author : Karthick Kumaran
+ * E-mail : asvkarthick@gmail.com
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    void serialize(TreeNode *root, std::string &s)
+    {
+        if (root == nullptr) {
+            s += "N";
+            return;
+        }
+        s += std::to_string(root->val) + ",";
+        serialize(root->left, s);
+        serialize(root->right, s);
+    }
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        std::string s = "";
+        if (root == nullptr) return s;
+        serialize(root, s);
+        return s;
+    }
+
+    int deserindex;
+    TreeNode* helperDeserialize(std::string &s)
+    {
+        if (deserindex >= s.size()) return nullptr;
+        if (s[deserindex] == 'N') {
+            deserindex++;
+            return nullptr;
+        }
+        int num = 0;
+        bool sign = false; // false for +ve, true for -ve
+        while (s[deserindex] != ',') {
+            if (s[deserindex] == '-') {
+                sign = true;
+            } else {
+                num = num * 10 + s[deserindex] - '0';
+            }
+            deserindex++;
+        }
+        deserindex++; // for comma
+        if (sign) num = -1 * num;
+
+        TreeNode *root = new TreeNode(num);
+        root->left = helperDeserialize(s);
+        root->right = helperDeserialize(s);
+        return root;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        deserindex = 0;
+        return helperDeserialize(data);
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
