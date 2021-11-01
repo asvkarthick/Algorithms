abcd5cc830ad857f7b394270014e97cd7cd959f6
Karthick Kumaran <asvkarthick@gmail.com>
2021-11-01 10:00:07 -0700

    Verifying an Alien Dictionary
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ Leetcode/program-953.cpp
@@ -0,0 +1,35 @@
+/* Problem: Verifying an Alien Dictionary
+ * Weblink: https://leetcode.com/problems/verifying-an-alien-dictionary/
+ * Author : Karthick Kumaran
+ * E-mail : asvkarthick@gmail.com
+ */
+
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        std::unordered_map<char, int> hashMap;
+
+        for (int i = 0; i < order.length(); i++) {
+            char ch = order[i];
+            hashMap[ch] = i;
+        }
+
+        std::string prevString = "";
+        int prevStringLen = 0, curStringLen = 0;
+        for (auto& str : words) {
+            int i = 0, j = 0;
+            curStringLen = str.length();
+            while (i < prevStringLen && j < curStringLen) {
+                if (hashMap[prevString[i]] < hashMap[str[j]]) break;
+                if (hashMap[prevString[i]] > hashMap[str[j]]) return false;
+                i++;
+                j++;
+            }
+            if (i == str.length() && prevStringLen > str.length()) return false;
+            prevStringLen = curStringLen;
+            prevString = str;
+        }
+
+        return true;
+    }
+};
