67e3e21243bd47fd8dd6173159063585a4e1e8b9
Karthick Kumaran <asvkarthick@gmail.com>
2021-11-01 00:14:30 -0700

    Flatten Binary Tree to a Linked List
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ Leetcode/program-114.cpp
@@ -0,0 +1,36 @@
+/* Problem: Flatten Binary Tree to a Linked List
+ * Weblink: https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+ * Author : Karthick Kumaran
+ * E-mail : asvkarthick@gmail.com
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode *node = root;
+
+        while (node != nullptr) {
+            if (node->left != nullptr) {
+                TreeNode *rightMost = node->left;
+                while (rightMost->right != nullptr) {
+                    rightMost = rightMost->right;
+                }
+                rightMost->right = node->right;
+                node->right = node->left;
+                node->left = nullptr;
+            }
+            node = node->right;
+        }
+    }
+};
