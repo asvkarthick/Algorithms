72cc07694fdd3a9a41d9234c5477e295cf5d36b9
Karthick Kumaran <asvkarthick@gmail.com>
2021-11-03 17:08:45 -0700

    Next Permutation
    Signed-off-by: Karthick Kumaran <asvkarthick@gmail.com>

---
+++ Leetcode/program-031.cpp
@@ -0,0 +1,36 @@
+/* Problem: Next Permutation
+ * Weblink: https://leetcode.com/problems/next-permutation/
+ * Author : Karthick Kumaran
+ * E-mail : asvkarthick@gmail.com
+ */
+
+class Solution {
+public:
+    void reverse(vector<int>& a, int start)
+    {
+    	int i = start, j = a.size() - 1;
+        while (i < j) {
+        	int t = a[i];
+        	a[i] = a[j];
+        	a[j] = t;
+        	i++;
+        	j--;
+        }
+    }
+
+    void nextPermutation(vector<int>& nums) {
+        int i = nums.size() - 1;
+        while (i > 0 && nums[i] <= nums[i - 1]) {
+            i--;
+        }
+        i--;
+        if (i >= 0) {
+            int j = nums.size() - 1;
+            while (nums[j] <= nums[i]) j--;
+            int t = nums[j];
+            nums[j] = nums[i];
+            nums[i] = t;
+        }
+        reverse(nums, i + 1);
+    }
+};
