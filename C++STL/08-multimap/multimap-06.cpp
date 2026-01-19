596b5a6423b18e3f8cc2af64faf3d5ea60dfe907
Karthick Kumaran <asvkarthick@gmail.com>
2026-01-19 10:00:04 -0800

    Program to find the closest points to origin

--- C++STL/08-multimap/multimap-06.cpp
+++ C++STL/08-multimap/multimap-06.cpp
@@ -8,7 +8,6 @@ std::multimap<double, int> closestPoints;
 
 std::vector<std::tuple<int, int>> findPclosest(std::vector<std::tuple<int, int>>& xyPlane, int P) {
     for (int i = 0; i < xyPlane.size(); i++) {
-        // auto distance = sqrt(xyPlane[i].first * xyPlane[i].first + xyPlane[i].second * xyPlane[i].second);
         double distance = sqrt(std::get<0>(xyPlane[i]) * std::get<0>(xyPlane[i]) + std::get<1>(xyPlane[i]) * std::get<1>(xyPlane[i]));
 	closestPoints.insert(std::pair(distance, i));
     }
@@ -25,7 +24,7 @@ std::vector<std::tuple<int, int>> findPclosest(std::vector<std::tuple<int, int>>
 int main(void) {
     std::vector<std::tuple<int, int>> xyPlane = {std::pair(1, 2), std::pair(1, 1), std::pair(3, 3), std::pair(-1, 2), std::pair(4, 4), std::pair(0, 0), std::pair(0, 4)};
 
-    auto v = findPclosest(xyPlane, 4);
+    auto v = findPclosest(xyPlane, xyPlane.size());
     for (int i = 0; i < v.size(); i++) {
         std::cout << std::get<0>(v[i]) << ", " << std::get<1>(v[i]) << std::endl;
     }
