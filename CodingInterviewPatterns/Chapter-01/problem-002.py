e03b5ae0b2ab6b7f3b75b778f9c8d7a665c2ee81
Karthick Kumaran <asvkarthick@gmail.com>
2026-03-22 16:55:43 -0700

    Add problem to pair sum

--- CodingInterviewPatterns/Chapter-01/problem-002.py
+++ CodingInterviewPatterns/Chapter-01/problem-002.py
@@ -1,3 +1,8 @@
+# Given an array of integers sorted in ascending order and a target value
+# return the indexes of the pair of numbers in that array that sum to the target.
+# The order of the indexes doesn't matter
+# If no pair is found, return an empty array
+
 def pair_sum_sorted(nums: list[int], target: int) -> list[int]:
     left, right = 0, len(nums) - 1
     while left < right:
