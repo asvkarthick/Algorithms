c4beee978d42b09abf445bbda3e918d4afa48c34
Karthick Kumaran <asvkarthick@gmail.com>
2026-03-22 16:55:27 -0700

    Add problem to the pair sum brute force

--- CodingInterviewPatterns/Chapter-01/problem-001.py
+++ CodingInterviewPatterns/Chapter-01/problem-001.py
@@ -1,3 +1,7 @@
+# Given an array of integers sorted in ascending order and a target value
+# return the indexes of the pair of numbers in that array that sum to the target.
+# The order of the indexes doesn't matter
+# If no pair is found, return an empty array
 
 def pair_sum_sorted_brute_force(nums: list[int], target: int) -> list[int]:
     n = len(nums)
