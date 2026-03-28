9e387eeafb8005eb6365498077fc984ae885977b
Karthick Kumaran <asvkarthick@gmail.com>
2026-03-28 12:30:08 -0700

    Add description of the largest container problem

--- CodingInterviewPatterns/Chapter-01/problem-006.py
+++ CodingInterviewPatterns/Chapter-01/problem-006.py
@@ -1,3 +1,9 @@
+# Largest Container
+# You are given an array of numbers, each representing the height of a vertical line
+# on a graph. A container can be formed with any pair of these lines, along the
+# x-axis of the graph.
+# Return the amount of water which the largest container can hold
+
 def max_volume(height: list[int]) -> int:
     left, right = 0, len(height) - 1
     maxvol = 0
